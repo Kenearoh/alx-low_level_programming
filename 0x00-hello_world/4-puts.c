a50706767380d9decfb573318669dd4cef8df954
Kenearoh <kenearoh12@gmail.com>
2023-03-16 16:10:32 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -7,8 +6,9 @@
  * Return: Always 0 (Success)
  */
 
-int main() {
+int main(void)
+{
 
-    puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 
-    return 0;
+return (0);
