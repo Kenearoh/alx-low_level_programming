cbfa31819eab4dd4447098b2b4d9e1a0a51f801a
Kenearoh <kenearoh12@gmail.com>
2023-03-19 16:18:06 -0700

    exclude some letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c = 97;
+
+	while (c <= 122)
+	{
+		if (c == 101 || c == 113)
+		{
+			c++;
+			continue;
+		}
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
