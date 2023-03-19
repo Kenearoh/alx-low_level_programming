468c111d83a1e9daeefeb9374c98e2a716eb0d02
Kenearoh <kenearoh12@gmail.com>
2023-03-19 14:42:05 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b = 97;
+
+	while (b <= 122)
+	{
+		putchar(b);
+			b++;
+	}
+	putchar('\n');
+	return (0);
+}
