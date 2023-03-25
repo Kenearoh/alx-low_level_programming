c43ca667d6e8dca9daf3b05af5ebf01aa3261986
Kenearoh <kenearoh12@gmail.com>
2023-03-25 06:44:00 -0700

    print straight line2

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,23 @@
 #include "main.h"
 /**
- * print_line - draws a straight line in the terminal
- * @n: number of times the character _ should be printed
- * Return: line should end with a \n, else only \n
+ * print_line - draws a straight line
+ * @n: number of times _ should be printed
+ * Return: Always 0
  */
 void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		_putchar('-');
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
