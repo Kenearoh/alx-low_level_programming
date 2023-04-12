1a4c7b81800e421def4742a0843e165f04c4323e
Kenearoh <kenearoh12@gmail.com>
2023-04-12 14:12:49 -0700

    arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Always zero
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+        printf("%d\n", argc - 1);
+        return (0);
+}
