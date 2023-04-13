1531f671110b3abc79bc3fd224375aad9e1df674
Kenearoh <kenearoh12@gmail.com>
2023-04-13 15:56:27 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,14 +12,18 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i;
 
-	array = (char*)malloc(size * sizeof(char));
+	array = malloc(sizeof(char) * size);
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else if (array == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
-		if (size == 0)
-		{
-			return (NULL);
-		}
 		array[i] = c;
 	}
 	return (array);
