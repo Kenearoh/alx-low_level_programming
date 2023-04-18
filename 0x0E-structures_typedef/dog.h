71d85f13b6c046a423df4ef96b7db457dff39581
kenearoh <kenearoh12@gmail.com>
2023-04-18 16:08:12 -0700

    new commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,26 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+* struct dog -  a type describing a dog
+* @name: First member
+* @age: Second member
+* @owner: Third member
+*
+*Longer description: name, age and owner of the dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+* dog_t - typedef for struct dog
+*/
+typedef struct dog dog_t;
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
