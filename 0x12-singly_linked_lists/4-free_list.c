cfdfc3894a528560278c8ab8f75c526cb85e9cf5
kenearoh <kenearoh12@gmail.com>
2023-04-28 12:33:37 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - Frees a list_t linked list
+ * @head: Pointer to the head of the list
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *list_ptr;
+
+	while (head != NULL)
+	{
+		list_ptr = head;
+		head = head->next;
+		free(list_ptr->str);
+		free(list_ptr);
+	}
+}
