2b9e3bdbea9d56d89ff0f1be86d6b1432ef245b9
Kenearoh <kenearoh12@gmail.com>
2023-04-28 05:35:33 -0700

    linked-list count

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to head of list_t list
+ * Return: number of elements found in linked list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t list_count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		list_count++;
+	}
+	return (list_count);
+}
